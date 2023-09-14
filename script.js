b05f45239f2f913f7822a2fe384648852f6a0b44
Marek Wegierek <mpwegierek@gmail.com>
2023-09-14 16:12:22 +0200

    Added features(creating items form external source and improved css)

--- script.js
+++ script.js
@@ -1,23 +1,59 @@
 const ingredientSelector = document.querySelectorAll(".ingredient-selector")
+const cardsOfDishes = document.querySelector(".dishes")
+import dishList from "./dishList.js"
 
 let forbiddenIngredients = []
+let isVegan = false
+
+/*Ingredients filter */
 
 ingredientSelector.forEach((button) => {
   button.addEventListener("click", () => {
     button.children[0].classList.toggle("invisible")
     button.children[1].classList.toggle("invisible")
 
-    if (!forbiddenIngredients.includes(button.id)) {
-      forbiddenIngredients.push(button.id)
+    if (button.id !== "vegan") {
+      if (!forbiddenIngredients.includes(button.id)) {
+        forbiddenIngredients.push(button.id)
+      } else {
+        forbiddenIngredients = forbiddenIngredients.filter(
+          (item) => item !== button.id
+        )
+      }
+      console.log(forbiddenIngredients)
     } else {
-      forbiddenIngredients = forbiddenIngredients.filter(
-        (item) => item !== button.id
-      )
+      isVegan = !isVegan
+      console.log(isVegan)
     }
-    console.log(forbiddenIngredients)
   })
 })
+/*End of Ingredients filter */
 
-/* let ass = [1, 2, 3, 4, 5, 6, 7, 8]
-console.log(ass.filter((item) => item !== 5))
- */
+/*Creating cards with dishes */
+
+let menuList = dishList.map((card) => {
+  let { img, title, ingredientsList } = card
+  return `<div class="dish-card">
+            <img class="dish-card-img" src=${img} />
+            <h4>${title}</h4>
+            <div class="dish-card-serving-time">
+              <img class="dish-card-icon" src="./Icons/sunrise.svg" />
+              <img class="dish-card-icon" src="./Icons/day.svg" />
+              <img class="dish-card-icon" src="./Icons/evening.svg" />
+            </div>
+            <div class="dish-card-ingredients">
+              <img class="dish-card-icon" src="./Icons/nuts.svg" />
+              <img class="dish-card-icon" src="./Icons/gluten.svg" />
+              <img class="dish-card-icon" src="./Icons/sugar.svg" />
+              <img class="dish-card-icon" src="./Icons/vegan.svg" />
+            </div>
+            <div class="dish-card-ingredient">
+              <h5>Ingredients:</h5>
+              <p class="dish-card-ingredients-list">
+                ${ingredientsList.join(", ")}
+              </p>
+            </div>
+          </div>`
+})
+
+cardsOfDishes.innerHTML = menuList.join("")
