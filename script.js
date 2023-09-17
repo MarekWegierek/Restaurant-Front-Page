b2a422a7e7367bb13044c2a9c2e959878d556482
Marek Węgierek <mpwegierek@gmail.com>
2023-09-17 12:30:04 +0200

    Added filter methods for allergens in cards

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const ingredientSelector = document.querySelectorAll(".ingredient-selector")
-const cardsOfDishes = document.querySelector(".dishes")
-import dishList from "./dishList.js"
+const ingredientSelector = document.querySelectorAll('.ingredient-selector')
+const cardsOfDishes = document.querySelector('.dishes')
+import dishList from './dishList.js'
 
 let havePeanuts = true
 let haveGluten = true
@@ -10,27 +10,77 @@ let isVegan = true
 /*Ingredients filter */
 
 ingredientSelector.forEach((button) => {
-  button.addEventListener("click", () => {
-    button.children[0].classList.toggle("invisible")
-    button.children[1].classList.toggle("invisible")
+  button.addEventListener('click', () => {
+    button.children[0].classList.toggle('invisible')
+    button.children[1].classList.toggle('invisible')
 
     switch (button.id) {
-      case "peanuts":
+      case 'peanuts':
         havePeanuts = !havePeanuts
         break
-      case "gluten":
+      case 'gluten':
         haveGluten = !haveGluten
         break
-      case "sugar":
+      case 'sugar':
         haveSugar = !haveSugar
         break
-      case "vegan":
+      case 'vegan':
         isVegan = !isVegan
         break
     }
     let menuList = dishList.map((card) => {
       let { img, title, ingredientsList } = card
 
+      let [peanuts, peanutsIconElem] = [
+        '',
+        `<img class="dish-card-icon" title="With Peanuts" src="./Icons/nuts.svg" />`,
+      ]
+      let [gluten, glutenIconElem] = [
+        '',
+        `<img class="dish-card-icon" title="With Gluten" src="./Icons/gluten.svg" />`,
+      ]
+      let [sugar, sugarIconElem] = [
+        '',
+        `<img class="dish-card-icon" title="With Sugar" src="./Icons/sugar.svg" />`,
+      ]
+      let [vegan, veganIconElem] = [
+        '',
+        '<img class="dish-card-icon green" title="Vegan" src="./Icons/vegan.svg" />',
+      ]
+
+      function testHavePeanuts(value, elem) {
+        if (value) {
+          peanuts = elem
+        } else {
+          peanuts = ''
+        }
+      }
+      function testHaveGluten(value, elem) {
+        if (value) {
+          gluten = elem
+        } else {
+          gluten = ''
+        }
+      }
+      function testHaveSugar(value, elem) {
+        if (value) {
+          sugar = elem
+        } else {
+          sugar = ''
+        }
+      }
+      function testIsVegan(value, elem) {
+        if (value) {
+          vegan = elem
+        } else {
+          vegan =
+            '<img class="dish-card-icon" title="Vegetarian" src="./Icons/no-vegan.svg" />'
+        }
+      }
+      testIsVegan(card.isVegan, veganIconElem)
+      testHaveSugar(card.haveSugar, sugarIconElem)
+      testHaveGluten(card.haveGluten, glutenIconElem)
+      testHavePeanuts(card.havePeanuts, peanutsIconElem)
       if (
         card.isVegan == isVegan &&
         card.havePeanuts == havePeanuts &&
@@ -38,7 +88,7 @@ ingredientSelector.forEach((button) => {
         card.haveSugar == haveSugar
       ) {
         return `
-        <div class="dish-card">
+          <div class="dish-card">
             <div class="dish-card-header">
               <img
                 class="dish-card-header-img"
@@ -53,23 +103,23 @@ ingredientSelector.forEach((button) => {
                 <img class="dish-card-icon" src="./Icons/evening.svg" />
               </div>
               <div class="dish-card-ingredients">
-                <img class="dish-card-icon" src="./Icons/nuts.svg" />
-                <img class="dish-card-icon" src="./Icons/gluten.svg" />
-                <img class="dish-card-icon" src="./Icons/sugar.svg" />
-                <img class="dish-card-icon" src="./Icons/vegan.svg" />
+                ${peanuts}
+                ${gluten}
+                ${sugar}
+                ${vegan}
               </div>
             </div>
             <div class="dish-card-ingredient">
               <h5>Ingredients:</h5>
               <p class="dish-card-ingredients-list">
-                ${ingredientsList.join(", ")}
+                ${ingredientsList.join(', ')}
               </p>
             </div>
           </div>
         `
       }
     })
-    cardsOfDishes.innerHTML = menuList.join("")
+    cardsOfDishes.innerHTML = menuList.join('')
   })
 })
 /*End of Ingredients filter */
@@ -77,6 +127,57 @@ ingredientSelector.forEach((button) => {
 /*Creating cards with dishes */
 let menuList = dishList.map((card) => {
   let { img, title, ingredientsList } = card
+
+  let [peanuts, peanutsIconElem] = [
+    '',
+    `<img class="dish-card-icon" title="With Peanuts" src="./Icons/nuts.svg" />`,
+  ]
+  let [gluten, glutenIconElem] = [
+    '',
+    `<img class="dish-card-icon" title="With Gluten" src="./Icons/gluten.svg" />`,
+  ]
+  let [sugar, sugarIconElem] = [
+    '',
+    `<img class="dish-card-icon" title="With Sugar" src="./Icons/sugar.svg" />`,
+  ]
+  let [vegan, veganIconElem] = [
+    '',
+    '<img class="dish-card-icon" title="Vegan" src="./Icons/vegan.svg" />',
+  ]
+
+  function testHavePeanuts(value, elem) {
+    if (value) {
+      peanuts = elem
+    } else {
+      peanuts = ''
+    }
+  }
+  function testHaveGluten(value, elem) {
+    if (value) {
+      gluten = elem
+    } else {
+      gluten = ''
+    }
+  }
+  function testHaveSugar(value, elem) {
+    if (value) {
+      sugar = elem
+    } else {
+      sugar = ''
+    }
+  }
+  function testIsVegan(value, elem) {
+    if (value) {
+      vegan = elem
+    } else {
+      vegan =
+        '<img class="dish-card-icon" title="Vegetarian" src="./Icons/no-vegan.svg" />'
+    }
+  }
+  testIsVegan(card.isVegan, veganIconElem)
+  testHaveSugar(card.haveSugar, sugarIconElem)
+  testHaveGluten(card.haveGluten, glutenIconElem)
+  testHavePeanuts(card.havePeanuts, peanutsIconElem)
 
   return `
           <div class="dish-card">
@@ -94,19 +195,19 @@ let menuList = dishList.map((card) => {
                 <img class="dish-card-icon" src="./Icons/evening.svg" />
               </div>
               <div class="dish-card-ingredients">
-                <img class="dish-card-icon" src="./Icons/nuts.svg" />
-                <img class="dish-card-icon" src="./Icons/gluten.svg" />
-                <img class="dish-card-icon" src="./Icons/sugar.svg" />
-                <img class="dish-card-icon" src="./Icons/vegan.svg" />
+                ${peanuts}
+                ${gluten}
+                ${sugar}
+                ${vegan}
               </div>
             </div>
             <div class="dish-card-ingredient">
               <h5>Ingredients:</h5>
               <p class="dish-card-ingredients-list">
-                ${ingredientsList.join(", ")}
+                ${ingredientsList.join(', ')}
               </p>
             </div>
           </div>
         `
 })
-cardsOfDishes.innerHTML = menuList.join("")
+cardsOfDishes.innerHTML = menuList.join('')
